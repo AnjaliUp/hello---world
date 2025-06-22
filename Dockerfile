f74876e27849ee7f3c63d2d3f1a2527bc8789947
AnjaliUp <upadhyayangel9999@gmail.com>
2025-06-22 14:48:33 +0530

    Done

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+docker build -t mlops .
+docker run -dp 127.0.0.1:8501:8501  mlops
--- demo.py
+++ demo.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """Module to calculate the area of a circle."""
 
 def area_of_circle(radius):
@@ -14,13 +13,12 @@ def area_of_circle(radius):
     pi = 3.14
     return pi * radius * radius
 
+
 def main():
     """Main function to print area of a circle with radius 10."""
     area = area_of_circle()
     print(area)
 
+
 if __name__ == "__main__":
     main()
-=======
-print("hello world")
->>>>>>> 3f1519c46e24a9ab7f38fb0e829fb844f07cbc3f
